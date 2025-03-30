8d19d1c3ff8b5282a1ba963f624ff9586ef3659c
dhivya-kumaravel <jodhivne22@gmail.com>
2025-03-30 18:43:54 +0530

    second changed

--- src/Pages/CreateJob.jsx
+++ src/Pages/CreateJob.jsx
@@ -11,7 +11,7 @@ const CreateJob = () => {
         const onSubmit = (data) => {
             data.skills = selectedOption;
             // console.log(data);
-            fetch("http://localhost:5000/post-job", {
+            fetch("jobs.json", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(data)
